751c015a5255aa2a3016b68fe7dda632e63dc652
Chris Foster <chriswfoster@gmail.com>
2018-06-22 12:35:03 -0500

    Created hello List

--- Program.cs
+++ Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace myApp
+
+namespace list_quickstart
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+             var names = new List<string> { "Chris", "Ana", "Felipe" };
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
         }
     }
 }
